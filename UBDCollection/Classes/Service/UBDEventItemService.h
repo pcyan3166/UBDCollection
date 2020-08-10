c13de161df7807fafeb1f0d969a7fe6e9749ada4
pcyan <yanpengchao@hotmail.com>
2020-08-10 23:55:24 +0800

    基本的访问用户行为日志数据的数据接口已经完成了

--- UBDCollection/Classes/Service/UBDEventItemService.h
+++ UBDCollection/Classes/Service/UBDEventItemService.h
@@ -11,6 +11,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @class UBDEventItem;
 typedef void(^GetEventsDataResultBlock)(NSArray<UBDEventItem *> *items, BOOL hasMore);
+typedef void(^AddEventsDataResultBlock)(UBDEventItem *items);
 
 /// 管理存放和获取行为日志的服务
 @interface UBDEventItemService : NSObject
@@ -22,40 +23,38 @@ typedef void(^GetEventsDataResultBlock)(NSArray<UBDEventItem *> *items, BOOL has
 /// @param item 日志条目
 + (void)addEvent:(UBDEventItem *)item;
 
+/// 添加一条行为日志
+/// @param item 日志条目
+/// @param result 添加成功后的回调（可以在这里获取到eId）
++ (void)addEvent:(UBDEventItem *)item withResult:(AddEventsDataResultBlock)result;
+
 /// 删除一条行为日志
 /// @param eId 指定日志Id
 + (void)removeEventWithId:(NSInteger)eId;
-
-/// 获取指定数量的行为日志信息
-/// @param count 指定数量
-/// @param resultBlock 异步结果回调
-+ (void)getEventsWithCount:(NSUInteger)count
-            andResultBlock:(GetEventsDataResultBlock)resultBlock;
-
-/// 根据Id获取行为日志信息
-/// @param ids 指定Ids
-/// @param resultBlock 异步结果回调
-+ (void)getEventsWithIds:(NSArray<NSNumber *> *)ids
-          andResultBlock:(GetEventsDataResultBlock)resultBlock;
 
 /// 清除所有日志
 + (void)clearEvents;
 
 /// 分页获取数据时，获取上一页数据（结果按自增Id倒序排列，所以这里是获取时间戳更早的数据）
 /// @param eId 当前数据的最大Id，返回的数据比这个Id更大
+/// @param isDesc 是否按照入库顺序的倒序排列
 /// @param count 要获取的数据条目数
 /// @param resultBlock 异步结果回调
 + (void)getPreviousPageEventsWithPartitionEventId:(NSInteger)eId
+                                            order:(BOOL)isDesc
                                      andPageCount:(NSUInteger)count
                                    andResultBlock:(GetEventsDataResultBlock)resultBlock;
 
 /// 分页获取数据时，获取下一页数据（结果按自增Id倒序排列，所以这里是获取时间戳更晚的数据）
 /// @param eId 当前数据的最小Id，返回的数据比这个Id更小
+/// @param isDesc 是否按照入库顺序的倒序排列
 /// @param count 要获取的数据条目数
 /// @param resultBlock 异步结果回调
 + (void)getNextPageEventsWithPartitionEventId:(NSInteger)eId
+                                        order:(BOOL)isDesc
                                  andPageCount:(NSUInteger)count
                                andResultBlock:(GetEventsDataResultBlock)resultBlock;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- UBDCollection/Classes/Service/UBDEventItemService.m
+++ UBDCollection/Classes/Service/UBDEventItemService.m
@@ -29,40 +29,140 @@
                      item.extraInfo ? item.extraInfo : @"''", item.sendStatus, item.realTime, item.ts];
     [[UBDDatabaseService shareInstance].databaseQueue inDatabase:^(FMDatabase * _Nonnull db) {
         [db executeUpdate:sql];
+    }];
+}
+
++ (void)addEvent:(UBDEventItem *)item withResult:(AddEventsDataResultBlock)result {
+    NSString *sql = [NSString stringWithFormat:@"insert into t_events \
+                     (moduleId, pageId, eventId, eventType, pLevel, extraInfo, sendStatus, realTime, ts) values (%ld, %ld, %ld, %d, %ld, %@, %lu, %d, %ld)",
+                     item.moduleId, item.pageId, item.eventId, item.eventType, item.pLevel,
+                     item.extraInfo ? item.extraInfo : @"''", item.sendStatus, item.realTime, item.ts];
+    [[UBDDatabaseService shareInstance].databaseQueue inDatabase:^(FMDatabase * _Nonnull db) {
+        [db executeUpdate:sql];
         
         FMResultSet *set = [db executeQuery:@"select * from t_events where rowid = last_insert_rowid()"];
         if ([set next]) {
             item.eId = [set intForColumn:@"eId"];
+            if (result) {
+                result(item);
+            }
+        } else {
+            if (result) {
+                result(item);
+            }
         }
     }];
 }
 
 + (void)removeEventWithId:(NSInteger)eId {
-    //
-}
-
-+ (void)getEventsWithCount:(NSUInteger)count andResultBlock:(GetEventsDataResultBlock)resultBlock {
-    //
-}
-
-+ (void)getEventsWithIds:(NSArray<NSNumber *> *)ids andResultBlock:(GetEventsDataResultBlock)resultBlock {
-    //
+    NSString *sql = [NSString stringWithFormat:@"delete from t_events where eId = %ld", eId];
+    [[UBDDatabaseService shareInstance].databaseQueue inDatabase:^(FMDatabase * _Nonnull db) {
+        [db executeUpdate:sql];
+    }];
 }
 
 + (void)clearEvents {
-    //
+    NSString *sql = @"delete from t_events";
+    [[UBDDatabaseService shareInstance].databaseQueue inDatabase:^(FMDatabase * _Nonnull db) {
+        [db executeUpdate:sql];
+    }];
 }
 
 + (void)getPreviousPageEventsWithPartitionEventId:(NSInteger)eId
+                                            order:(BOOL)isDesc
                                      andPageCount:(NSUInteger)count
                                    andResultBlock:(GetEventsDataResultBlock)resultBlock {
-    //
+    NSString *sql = [NSString stringWithFormat:@"select * from t_events order by eId %@ limit %lu",
+                     isDesc ? @"desc" : @"asc", count + 1];
+    if (eId > 0) {
+        if (isDesc) {
+            sql = [NSString stringWithFormat:@"select * from t_events where eId > %ld order by eId %@ limit %lu",
+                   eId, isDesc ? @"desc" : @"asc", count + 1];
+        } else {
+            sql = [NSString stringWithFormat:@"select * from t_events where eId < %ld order by eId %@ limit %lu",
+            eId, isDesc ? @"desc" : @"asc", count + 1];
+        }
+    }
+    [[UBDDatabaseService shareInstance].databaseQueue inDatabase:^(FMDatabase * _Nonnull db) {
+        FMResultSet *rs = [db executeQuery:sql];
+        
+        NSMutableArray *mArray = [NSMutableArray arrayWithCapacity:count];
+        while ([rs next]) {
+            UBDEventItem *item = [UBDEventItemService getEventItemFromResultSet:rs];
+            if (item != nil) {
+                [mArray addObject:item];
+            }
+        }
+        
+        if (resultBlock) {
+            BOOL hasMore = NO;
+            if (mArray.count > count) {
+                hasMore = YES;
+                [mArray removeObjectAtIndex:0];
+            }
+            
+            resultBlock(mArray, hasMore);
+        }
+    }];
 }
 
 + (void)getNextPageEventsWithPartitionEventId:(NSInteger)eId
+                                        order:(BOOL)isDesc
                                  andPageCount:(NSUInteger)count
                                andResultBlock:(GetEventsDataResultBlock)resultBlock {
-    //
+    NSString *sql = [NSString stringWithFormat:@"select * from t_events order by eId %@ limit %lu",
+                     isDesc ? @"desc" : @"asc", count + 1];
+    if (eId > 0) {
+        if (isDesc) {
+            sql = [NSString stringWithFormat:@"select * from t_events where eId < %ld order by eId %@ limit %lu",
+                   eId, isDesc ? @"desc" : @"asc", count + 1];
+        } else {
+            sql = [NSString stringWithFormat:@"select * from t_events where eId > %ld order by eId %@ limit %lu",
+            eId, isDesc ? @"desc" : @"asc", count + 1];
+        }
+    }
+    [[UBDDatabaseService shareInstance].databaseQueue inDatabase:^(FMDatabase * _Nonnull db) {
+        FMResultSet *rs = [db executeQuery:sql];
+        
+        NSMutableArray *mArray = [NSMutableArray arrayWithCapacity:count];
+        while ([rs next]) {
+            UBDEventItem *item = [UBDEventItemService getEventItemFromResultSet:rs];
+            if (item != nil) {
+                [mArray addObject:item];
+            }
+        }
+        
+        if (resultBlock) {
+            BOOL hasMore = NO;
+            if (mArray.count > count) {
+                hasMore = YES;
+                [mArray removeLastObject];
+            }
+            
+            resultBlock(mArray, hasMore);
+        }
+    }];
+}
+
++ (UBDEventItem *)getEventItemFromResultSet:(FMResultSet *)rs {
+    if (rs == nil) {
+        return nil;
+    }
+    
+    UBDEventItem *eventItem = [[UBDEventItem alloc] init];
+    eventItem.eId = [rs intForColumn:@"eId"];
+    eventItem.ts = [rs longLongIntForColumn:@"ts"];
+    eventItem.moduleId = [rs intForColumn:@"moduleId"];
+    eventItem.pageId = [rs intForColumn:@"pageId"];
+    eventItem.pLevel = [rs intForColumn:@"pLevel"];
+    eventItem.eventId = [rs intForColumn:@"eventId"];
+    eventItem.eventType = [rs intForColumn:@"eventType"];
+    eventItem.extraInfo = [rs stringForColumn:@"extraInfo"];
+    eventItem.sendStatus = [rs intForColumn:@"sendStatus"];
+    eventItem.realTime = [rs boolForColumn:@"realTime"];
+    eventItem.rId = [rs intForColumn:@"rId"];
+    
+    return eventItem;
 }
 
 @end
